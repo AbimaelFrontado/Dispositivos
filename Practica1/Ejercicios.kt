07139938f0f78df2e4effff699ad008b18c4a7b0
AbimaelFrontado <167887266+AbimaelFrontado@users.noreply.github.com>
2025-08-25 20:00:55 -0500

    Modificando para concordar con las convenciones de nomenclatura

--- Practica1/Ejercicios.kt
+++ Practica1/Ejercicios.kt
@@ -1,16 +1,15 @@
-//Primera parte: Evaluación Empleados
-
-fun EvaluarEmp() {
+// Primera parte: Evaluación Empleados
+fun evaluarEmp() {
     print("Ingrese su salario mensual: ")
-    val salario = readln().toDouble() //Leer la entrada y convertirla a double 
+    val salario = readln().toDouble()
 
     print("Ingrese su puntuación (0 - 10): ")
-    val puntuacion = readln().toInt() //leear la entrada y convertirla a int
+    val puntuacion = readln().toInt()
 
     var nivel = ""
     var dinero = 0.0
 
-    when (puntuacion) { //Especie de switch case con intervalos 
+    when (puntuacion) {
         in 0..3 -> {
             nivel = "Inaceptable"
             dinero = salario * (puntuacion / 10.0)
@@ -31,17 +30,15 @@ fun EvaluarEmp() {
 
     println("Nivel de Rendimiento: $nivel")
     println("Cantidad de Dinero Recibido: $${"%.2f".format(dinero)}")
-
 }
 
-//Segunda Parte: Piedra, Papel y Tijera
-
-fun PPT() {
+// Segunda Parte: Piedra, Papel y Tijera
+fun ppt() {
     val opciones = listOf("piedra", "papel", "tijera")
-    val computadora = opciones[Random.nextInt(opciones.size)] //tomando una posicion random del 0 al 2 y el valor de la lista se agrega a la varibale
+    val computadora = opciones[Random.nextInt(opciones.size)]
 
     print("Elija piedra, papel o tijera: ")
-    val usuario = readln().lowercase()   //leyendo y transformando a minúsculas
+    val usuario = readln().lowercase()
     println("Computadora eligió: $computadora")
 
     when {
@@ -50,19 +47,19 @@ fun PPT() {
         (usuario == "papel" && computadora == "piedra") ||
         (usuario == "tijera" && computadora == "papel") -> println("Resultado: Ganaste")
         usuario in opciones -> println("Resultado: Perdiste")
-        else -> println("Opción inválida.")
+        else -> println("Opción inválida ")
     }
 }
 
-//Tercera Parte: Calculadora 
-
-fun Numeros(): Pair<Double, Double> { //función auxiliar para la calculadora
+// Tercera Parte: Calculadora
+fun numeros(): Pair<Double, Double> {
     print("Ingrese el primer número: ")
-    val a = readLine().toDouble()  
+    val a = readln().toDouble()
     print("Ingrese el segundo número: ")
-    val b = readLine().toDouble()
+    val b = readln().toDouble()
     return Pair(a, b)
-} 
+}
+
 fun calculadora() {
     while (true) {
         println("\n==== Menú ====")
@@ -73,62 +70,60 @@ fun calculadora() {
         println("5. Salir")
         print("Seleccione una opción: ")
 
-        when (readLine()) {
+        when (readln()) {
             "1" -> {
-                val (a, b) = Numeros()
+                val (a, b) = numeros()
                 println("Resultado: ${a + b}")
             }
             "2" -> {
-                val (a, b) = Numeros()
+                val (a, b) = numeros()
                 println("Resultado: ${a - b}")
             }
             "3" -> {
-                val (a, b) = Numeros()
+                val (a, b) = numeros()
                 println("Resultado: ${a * b}")
             }
             "4" -> {
-                val (a, b) = Numeros()
+                val (a, b) = numeros()
                 if (b != 0.0) println("Resultado: ${a / b}")
                 else println("Error: División entre 0 no permitida")
             }
             "5" -> {
-                println("Saliendo de la calculadora...")
+                println("Saliendo de la calculadora ")
                 break
             }
-            else -> println("Opción inválida.")
+            else -> println("Opción inválida ")
         }
     }
 }
 
-
-//Parte Cuatro: Adivinar Número 
-
+// Parte Cuatro: Adivinar Número
 fun adivinar() {
-    val Secreto = Random.nextInt(1, 31) //Se Genera un número 
+    val secreto = Random.nextInt(1, 31)
     var intentos = 5
 
     println("He pensado un número entre 1 y 30. Tienes 5 intentos.")
 
     while (intentos > 0) {
         print("Ingresa tu número: ")
-        val intento = readLine().toInt() 
-        
+        val intento = readln().toInt()
+
         when {
-            intento == numeroSecreto -> {
-                println("  Adivinaste el número.")
+            intento == secreto -> {
+                println("Adivinaste el número.")
                 return
             }
-            intento < numeroSecreto -> println("El número secreto es mayor.")
+            intento < secreto -> println("El número secreto es mayor.")
             else -> println("El número secreto es menor.")
         }
         intentos--
         println("Te quedan $intentos intentos ")
     }
 
-    println("Game Over, el número era $Secreto")
+    println("Game Over, el número era $secreto")
 }
 
-//función main 
+// Función main
 fun main() {
     while (true) {
         println("\n==== Menú Principal ====")
@@ -140,16 +135,15 @@ fun main() {
         print("Seleccione una opción: ")
 
         when (readln()) {
-            "1" -> EvaluarEmp()
-            "2" -> PPT()
+            "1" -> evaluarEmp()
+            "2" -> ppt()
             "3" -> calculadora()
             "4" -> adivinar()
             "5" -> {
-                println("Saliendo del programa ")
+                println("Saliendo del programa")
                 break
             }
-            else -> println("Opción inválida ")
+            else -> println("Opción inválida")
         }
     }
-
-} 
+}
