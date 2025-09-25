4c0731d0639cdcf60b71b3cbcc4c6d6171999a94
AbimaelFrontado <167887266+AbimaelFrontado@users.noreply.github.com>
2025-09-25 14:35:18 -0500

    Creando el archivo OpcionesActivity.kt

---
+++ Practica4/Problema2/OpcionesActivity.kt
@@ -0,0 +1,20 @@
+/**
+ * Descripción del problema: Muestra la nota recibida y da opciones de compartir o editar.
+ * Autor: Abimael Ernesto Frontado Fajardo
+ * Fecha de creación: 25/09/2025
+ * Fecha de última modificación: 25/09/2025
+ */
+
+package com.abi.myapplication
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class OpcionesActivity : AppCompatActivity() {
+ 
+}
