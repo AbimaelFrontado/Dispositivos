e3c85221d51e5aa2c400499a90b545a01c57eb1e
AbimaelFrontado <167887266+AbimaelFrontado@users.noreply.github.com>
2025-09-25 14:12:05 -0500

    Creando el archivo EditorActivity.kt

---
+++ Practica4/Problema2/EditorActivity.kt
@@ -0,0 +1,21 @@
+/**
+ * Descripción del problema: Editor de notas que envía texto a una segunda pantalla.
+ * Autor: Abimael Ernesto Frontado Fajardo
+ * Fecha de creación: 25/09/2025
+ * Fecha de última modificación: 25/09/2025
+ */
+
+package com.abi.myapplication
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+ 
+
+class EditorActivity : AppCompatActivity() {
+ 
+}
