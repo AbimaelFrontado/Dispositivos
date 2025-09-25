cdf5ba6f1a930258618e119997ca76540810b508
AbimaelFrontado <167887266+AbimaelFrontado@users.noreply.github.com>
2025-09-25 14:03:38 -0500

    Creando el archivo ResumenActivity.kt básico

---
+++ Practica4/Problema1/ResumenActivity.kt
@@ -0,0 +1,19 @@
+/**
+ * Descripción del problema: Muestra un resumen del perfil y permite confirmar o volver a editar.
+ * Autor: Abimael Ernesto Frontado Fajardo
+ * Fecha de creación: 25/09/2025
+ * Fecha de última modificación: 25/09/2025
+ */
+
+package com.abi.myapplication
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ResumenActivity : AppCompatActivity() { 
+  
+}
