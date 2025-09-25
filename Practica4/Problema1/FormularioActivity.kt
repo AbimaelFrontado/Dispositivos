5c34383fe8764e5b14b132d9a28d82ed49d705d7
AbimaelFrontado <167887266+AbimaelFrontado@users.noreply.github.com>
2025-09-25 13:57:05 -0500

    creando  el archivo FormularioActivity.kt básico

---
+++ Practica4/Problema1/FormularioActivity.kt
@@ -0,0 +1,24 @@
+/**
+ * Descripción del problema: Formulario de perfil que envía datos a una segunda pantalla.
+ * Autor: Abimael Ernesto Frontado Fajardo
+ * Fecha de creación: 25/09/2025
+ * Fecha de última modificación: 25/09/2025
+ *
+ * NOTA: La clase 'AppCompatActivity' y las dependencias de AndroidX son necesarias para usar 'registerForActivityResult'.
+ */
+
+package com.abi.myapplication
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import java.io.Serializable
+ 
+class FormularioActivity : AppCompatActivity() {
+ 
+}
